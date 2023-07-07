634a4fdb84d64dcb9a1f4b0f1c5b90ce44dd7224
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-07-07 21:46:04 +0530

    feat: :sparkles: add custom fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,58 +1,83 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
 import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
+	DarkTheme,
+	DefaultTheme,
+	ThemeProvider,
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
 import { useColorScheme } from "react-native";
+import {
+	Quicksand_300Light,
+	Quicksand_400Regular,
+	Quicksand_500Medium,
+	Quicksand_600SemiBold,
+	Quicksand_700Bold,
+} from "@expo-google-fonts/quicksand";
+import { FiraCode_400Regular } from "@expo-google-fonts/fira-code";
+import {
+	JetBrainsMono_400Regular,
+	JetBrainsMono_400Regular_Italic,
+} from "@expo-google-fonts/jetbrains-mono";
+import {
+	SourceCodePro_400Regular,
+	SourceCodePro_400Regular_Italic,
+} from "@expo-google-fonts/source-code-pro";
 
 export { ErrorBoundary } from "expo-router";
 
 export const unstable_settings = {
-  initialRouteName: "index",
+	initialRouteName: "index",
 };
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-    ...FontAwesome.font,
-  });
+	const [loaded, error] = useFonts({
+		Quicksand_300Light,
+		Quicksand_400Regular,
+		Quicksand_500Medium,
+		Quicksand_600SemiBold,
+		Quicksand_700Bold,
+		FiraCode_400Regular,
+		JetBrainsMono_400Regular,
+		JetBrainsMono_400Regular_Italic,
+		SourceCodePro_400Regular,
+		SourceCodePro_400Regular_Italic,
+	});
 
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+	useEffect(() => {
+		if (error) throw error;
+	}, [error]);
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+	useEffect(() => {
+		if (loaded) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
+	if (!loaded) {
+		return null;
+	}
 
-  return <RootLayoutNav />;
+	return <RootLayoutNav />;
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+	const colorScheme = useColorScheme();
 
-  return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen
-          name="index"
-          options={{
-            headerShown: false,
-          }}
-        />
-      </Stack>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider
+			value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+		>
+			<Stack>
+				<Stack.Screen
+					name="index"
+					options={{
+						headerShown: false,
+					}}
+				/>
+			</Stack>
+		</ThemeProvider>
+	);
 }
