5ac74fe2f811a04f2ab46c60d823250f3f2199b0
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-07-07 08:22:05 +0530

    feat: :sparkles: add react native text editor

--- src/screens/home-screen/home-screen.tsx
+++ src/screens/home-screen/home-screen.tsx
@@ -2,9 +2,13 @@ import { SafeAreaView, Text } from "react-native";
 import { darkStyles, lightStyles } from "./styles";
 import { StatusBar } from "expo-status-bar";
 import { useThemeStore } from "@/src/store";
+import { useState } from "react";
+import CodeEditor, { CodeEditorSyntaxStyles } from "@rivascva/react-native-code-editor";
 
 const HomeScreen = () => {
 	const { theme } = useThemeStore();
+
+	const [code, setCode] = useState<string>("");
 
 	return (
 		<SafeAreaView
@@ -12,9 +16,7 @@ const HomeScreen = () => {
 				theme === "dark" ? darkStyles.Container : lightStyles.Container
 			}
 		>
-			<Text style={theme === "dark" ? darkStyles.Text : lightStyles.Text}>
-				Home Screen
-			</Text>
+			
 			<StatusBar style={theme === "dark" ? "light" : "dark"} />
 		</SafeAreaView>
 	);
