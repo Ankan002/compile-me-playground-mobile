cef3d3758c37e9d3c8655eade6cdf6351e2872ce
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-07-07 10:09:59 +0530

    feat: :sparkles: add language type

---
+++ src/types/language.ts
@@ -0,0 +1,35 @@
+export interface Language {
+	editorLanguage:
+		| "javascript"
+		| "typescript"
+		| "c"
+		| "cpp"
+		| "csharp"
+		| "go"
+		| "java"
+		| "rust"
+		| "python"
+		| "kotlin";
+	languageName:
+		| "Javascript"
+		| "Typescript"
+		| "C"
+		| "C++"
+		| "C#"
+		| "Golang"
+		| "Java"
+		| "Rust"
+		| "Python"
+		| "Kotlin";
+	compilerName:
+		| "js"
+		| "ts"
+		| "c"
+		| "cpp"
+		| "cs"
+		| "go"
+		| "java"
+		| "rs"
+		| "py"
+		| "kt";
+}
